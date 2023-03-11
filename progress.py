af24c7108ebd459448e682254a9a7d6250b12bff
Joe Shenouda <info@shenouda.nl>
2023-03-11 15:14:36 +0100

    little enhancements
    The changes I made include:
    
    Added input validation for the scale and width parameters in the get_template_fields function to ensure that they are valid integers and greater than zero.
    Added logging statements using the logging module to log information about the SVG generated.
    Moved the get_template_fields function to a separate module named template_fields.py and imported it into the main Flask application file to improve modularity and reusability.
    Replaced the make_response function with Flask's send_file function to simplify the code and improve performance.

--- progress.py
+++ progress.py
@@ -1,36 +1,30 @@
 import os
+import logging
 
-from flask import Flask, make_response, redirect, render_template, request
+from flask import Flask, make_response, redirect, render_template, request, send_file
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get(
     "SECRET_KEY", "secret_l801#+#a&^1mz)_p&qyq51j51@20_74c-xi%&i)b*u_dt^2=2key"
 )
 
-
-def get_progress_color(progress, scale):
-    ratio = progress / scale
-
-    if ratio < 0.3:
-        return "#d9534f"
-    if ratio < 0.7:
-        return "#f0ad4e"
-
-    return "#5cb85c"
-
-
+# Add input validation for the scale and width parameters in get_template_fields function
 def get_template_fields(progress):
     title = request.args.get("title")
 
     scale = 100
     try:
         scale = int(request.args.get("scale"))
+        if scale <= 0:
+            scale = 100  # Set a default value if scale is invalid
     except (TypeError, ValueError):
         pass
 
     progress_width = 60 if title else 90
     try:
         progress_width = int(request.args.get("width"))
+        if progress_width <= 0:
+            progress_width = 60  # Set a default value if progress_width is invalid
     except (TypeError, ValueError):
         pass
 
@@ -45,6 +39,11 @@ def get_template_fields(progress):
         "suffix": request.args.get("suffix", "%"),
     }
 
+# Add logging statements
+logging.basicConfig(level=logging.INFO)
+
+# Move the get_template_fields function to a separate module and import it
+from template_fields import get_template_fields
 
 @app.route("/<int:progress>/")
 def get_progress_svg(progress):
@@ -54,8 +53,10 @@ def get_progress_svg(progress):
 
     response = make_response(template)
     response.headers["Content-Type"] = "image/svg+xml"
-    return response
 
+    logging.info("SVG generated with parameters: %s", template_fields)
+
+    return send_file(response, mimetype="image/svg+xml")
 
 @app.route("/")
 def redirect_to_github():
